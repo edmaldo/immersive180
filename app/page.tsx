b1461304d94a56b9a697b538df3a88c8de216a8c
Eddie <ed_maldo@protonmail.com>
2026-05-13 17:12:42 -0600

    Initial Immersive180 Setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold">Immersive180</h1>
+        <p className="mt-4 text-xl text-gray-400">
+          The future of immersive VR storytelling.
+        </p>
+      </div>
+    </main>
+  );
+}
